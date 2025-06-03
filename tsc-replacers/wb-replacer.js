da965056b995f310c71fa35fd70df4d48959c078
Wired Home <212203636+wihome-dev@users.noreply.github.com>
2025-06-03 17:30:30 +0300

    Fix 'wb-replacer' regex pattern

--- tsc-replacers/wb-replacer.js
+++ tsc-replacers/wb-replacer.js
@@ -1,5 +1,5 @@
 exports.default = function moduleReplacer({ orig }) {
     return orig
         .replace('../wb-rules-modules/', '')
-        .replace(/\.js^/, '');
+        .replace(/\.js'$/i, '\'');
 }
