dc4cd4924a0c29d2cd8b4b5248c126f2ecc308f3
abatalev <andrey.batalev@gmail.com>
2025-06-05 00:47:53 +0300

    Remove Babel (#1)
    * Remove Babel
    
    * revert wb-replacer.js
    
    * delete files dist/_virtual/*
    
    * remote tsc-alias
    
    * Cleanup and rearrange changes
    
    ---------
    
    Co-authored-by: Wired Home <212203636+wihome-dev@users.noreply.github.com>

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,6 @@
 import multi from '@rollup/plugin-multi-entry'
 import typescript from '@rollup/plugin-typescript'
+import tscAlias from 'rollup-plugin-tsc-alias'
 import dotenv from '@dotenv-run/rollup'
 import del from 'rollup-plugin-delete'
 
@@ -8,15 +9,25 @@ export default {
   plugins: [
     multi({ preserveModules: true }),
     typescript(),
+    tscAlias(),
     dotenv({
       prefix: '^APP_',
       verbose: false
     }),
-    del({ targets: ['build/*', 'dist/*'] })
+    del({
+      targets: 'dist/*',
+      hook: 'buildStart',
+      verbose: false
+    }),
+    del({
+      targets: 'dist/_virtual',
+      hook: 'closeBundle',
+      verbose: false
+    })
   ],
   output: {
-    format: 'es',
-    dir: 'build',
+    format: 'cjs',
+    dir: 'dist',
     preserveModules: true
   }
 }
