7a2c161b7cbeae69e618d3b866245cb9ae4c06dc
Wired Home <212203636+wihome-dev@users.noreply.github.com>
2025-06-08 01:53:33 +0300

    Update wb-replacer.js (#6)

--- tsc-replacers/wb-replacer.js
+++ tsc-replacers/wb-replacer.js
@@ -1,5 +1,9 @@
 exports.default = function moduleReplacer({ orig }) {
   return orig
+    // Случай импорта в правилах
     .replace('../wb-rules-modules/', '')
-    .replace(/\.js'/i, '\'')
+    // Случай импорта в модулях правил
+    .replace('\'./', '\'')
+    // Расширение, появляющееся в процессе работы Rollup
+    .replace('.js\'', '\'');
 }
