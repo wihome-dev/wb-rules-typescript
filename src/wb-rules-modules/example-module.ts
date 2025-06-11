6c6b1a2e3116bf7d59e1d71c929f87cf3c8e6ce7
Wired Home <212203636+wihome-dev@users.noreply.github.com>
2025-06-11 10:17:26 +0300

    Add Event module

--- src/wb-rules-modules/example-module.ts
+++ src/wb-rules-modules/example-module.ts
@@ -1,3 +1,5 @@
+import { useEvent } from '@wbm/event'
+
 export const HELLO_MESSAGE = 'Hello from TypeScript version!'
 
 // Экспортируем стрелочную функцию.
@@ -7,6 +9,23 @@ export const sum = (a: number, b: number) => a + b
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const mult = (a: number, b: number) => a * b
 
-export const useExample = (options: { name: string, secret: string }) => ({
-  getName: () => options.name
-})
+export const useExample = (options: { name: string, secret?: string }) => {
+  const stateChangedEvent = useEvent<number>()
+
+  let step = 0
+
+  setTimeout(() => {
+    step += 1
+    stateChangedEvent.raise(step)
+  }, 2000)
+
+  setTimeout(() => {
+    step += 1
+    stateChangedEvent.raise(step)
+  }, 4000)
+
+  return {
+    getName: () => options.name,
+    onStateChanged: stateChangedEvent.on
+  }
+}
---
+++ tests/wb-rules-modules/event.test.ts
@@ -0,0 +1,93 @@
+import { useEvent } from '@wbm/event'
+
+test('Handle it', (done) => {
+  const testEvent = useEvent<string>()
+  const testMessage = 'OK'
+
+  // Подписка на событие.
+  testEvent.on((message) => {
+    expect(message).toBe(testMessage)
+    done()
+  })
+
+  // Вызов события.
+  testEvent.raise(testMessage)
+})
+
+test('Handle it once', () => {
+  const testEvent = useEvent()
+  let count = 0
+
+  // Подписка на событие.
+  testEvent.once(() => {
+    count += 1
+  })
+
+  // Двойной вызов события.
+  testEvent.raise()
+  testEvent.raise()
+
+  expect(count).toBe(1)
+})
+
+test('Unsubscribe, type #1', () => {
+  const testEvent = useEvent()
+  let count = 0
+
+  // Подписка на событие.
+  const listener = testEvent.on(() => {
+    count += 1
+  })
+
+  // Вызов события.
+  testEvent.raise()
+
+  // Отписка от события.
+  listener.off()
+
+  // Вызов события.
+  testEvent.raise()
+
+  expect(count).toBe(1)
+})
+
+test('Unsubscribe, type #2', () => {
+  const testEvent = useEvent()
+  let count = 0
+
+  const callback = () => count += 1
+
+  // Подписка на событие.
+  testEvent.on(callback)
+
+  // Вызов события.
+  testEvent.raise()
+
+  // Отписка от события.
+  testEvent.off(callback)
+
+  // Вызов события.
+  testEvent.raise()
+
+  expect(count).toBe(1)
+})
+
+test('Multiple handlers', () => {
+  const testEvent = useEvent()
+  let count = 0
+
+  // Подписка на событие.
+  testEvent.on(() => {
+    count += 1
+  })
+
+  // Ещё одна подписка на событие.
+  testEvent.on(() => {
+    count += 1
+  })
+
+  // Вызов события.
+  testEvent.raise()
+
+  expect(count).toBe(2)
+})
