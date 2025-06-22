074511b0242a428352bd9b1c4bd367ab58a19312
Wired Home <212203636+wihome-dev@users.noreply.github.com>
2025-06-22 17:20:02 +0300

    Fix Moes Switch action handling (#27)

--- src/wb-rules-modules/core/wrappers.ts
+++ src/wb-rules-modules/core/wrappers.ts
@@ -2,13 +2,20 @@
  * Позволяет получить объект для работы с указанным устройством.
  * @param deviceId Идентификатор устройства.
  */
-export function getDeviceSafe(deviceId: string) {
+export function getDeviceSafe(
+  deviceId: string,
+  isReadyFunc = () => true
+) {
   let device: Device | undefined
 
   return {
     /** Возвращает существующий объект или пытается найти, если его ещё нет. */
     get safe() {
-      return device ??= getDevice(deviceId)
+      return device ?? (
+        isReadyFunc()
+          ? device = getDevice(deviceId)
+          : undefined
+      )
     }
   }
 }
@@ -18,13 +25,21 @@ export function getDeviceSafe(deviceId: string) {
  * @param deviceId Идентификатор устройства.
  * @param controlId Идентификатор контрола.
  */
-export function getControlSafe(deviceId: string, controlId: string) {
+export function getControlSafe(
+  deviceId: string,
+  controlId: string,
+  isReadyFunc = () => true
+) {
   let control: Cell | undefined
 
   return {
     /** Возвращает существующий объект или пытается найти, если его ещё нет. */
     get safe() {
-      return control ??= getControl(`${deviceId}/${controlId}`)
+      return control ?? (
+        isReadyFunc()
+          ? control = getControl(`${deviceId}/${controlId}`)
+          : undefined
+      )
     }
   }
 }
