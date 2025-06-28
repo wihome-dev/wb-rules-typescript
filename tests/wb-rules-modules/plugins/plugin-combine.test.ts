449601a2e52cd74c1f3e47921b6696a3ff5664ae
Wired Home <212203636+wihome-dev@users.noreply.github.com>
2025-06-28 14:06:40 +0300

    Fix plugin combining (#35)

---
+++ tests/wb-rules-modules/plugins/plugin-combine.test.ts
@@ -0,0 +1,27 @@
+import { defineZigbeeDevice } from '@wbm/define-device'
+
+function zeroPlugin() {
+  return () => ({
+    zero: 0
+  })
+}
+
+function twoPlugin() {
+  return () => ({
+    two: 2
+  })
+}
+
+test('Expect plugins to be combined', () => {
+  // Девайс-пустышка, для тестирования сборки
+  // с несколькими плагинами.
+  const device = defineZigbeeDevice('known-device', {
+    plugins: [
+      zeroPlugin(),
+      twoPlugin()
+    ]
+  })
+
+  expect(device.zero).toBe(0)
+  expect(device.two).toBe(2)
+})
